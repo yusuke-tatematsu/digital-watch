127f0f7d2fdc40c972ed0abc63b5a3057dd35480
yusuke-tatematsu <124574210+yusuke-tatematsu@users.noreply.github.com>
2023-02-06 17:33:05 +0700

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,31 @@
+let adjust = 0
+let time = ""
+input.onGesture(Gesture.Shake, function () {
+    let minutes = 0
+    let ampm = 0
+    let hours = 0
+    adjust = hours
+    if (ampm) {
+        if (hours > 12) {
+            adjust = hours - 12
+        } else {
+            if (hours == 0) {
+                adjust = 12
+            }
+        }
+    }
+    time = "" + adjust
+    time = "" + time + ":"
+    if (minutes < 10) {
+        time = "" + time + "0"
+    }
+    time = "" + time + minutes
+    if (ampm) {
+        if (hours > 11) {
+            time = "" + time + "PM"
+        } else {
+            time = "" + time + "AM"
+        }
+    }
+    basic.showString(time)
+})
